b7b36e8a333a9eafd7431c830aa413d454a826ac
Roy Tu <thatsnail@gmail.com>
2015-03-24 07:14:40 -0400

    Heavy optimization

--- awfuljokes.py
+++ awfuljokes.py
@@ -1,156 +1,145 @@
 import string
 import wikipedia
 import re
-import math
-import random
+from math import floor
+from random import random
 
 VOWELS = "AEIOUYaeiouy"
 CONSONANTS = "BCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz"
-MIN_PHENOMES_SHARED = 3
+MIN_PHENOMES_SHARED = 2
 
-def getPhenomes(word):
-	word = word.upper();
-	result = "Word not found."
-	refFile = open("phoneme_ref.txt");
-	for line in refFile.read().splitlines():
-		if line.find(word, 0, len(word)) > -1:
-			result = line
-			break
-	refFile.close();
-	# clean result
-	result = result[len(word)+1:]
-	return result
+class Words(object):
+    """ A class for retrieving nouns and the like
+    """
+    def __init__(self):
+        self.__nouns = []
+        self.__phonemeDict = {}
+
+    @property
+    def nouns(self):
+        if not self.__nouns:
+            f = open("nouns")
+            self.__nouns = f.read().splitlines()
+            f.close()
+        return self.__nouns
+
+    def getRandomNoun(self):
+        """ Return a random noun
+        """
+        return self.nouns[int(floor(random() * len(self.nouns)))]
+
+    def phonemes(self, word):
+        """ Given a word, return its phonemes
+        """
+        if not self.__phonemeDict:
+            f = open("phoneme_ref.txt")
+            self.__phonemeDict = {}
+            for line in f.read().splitlines():
+                k, v = line.split("\t")
+                self.__phonemeDict[k.lower()] = v
+            f.close()
+        return self.__phonemeDict[word.lower()]
+
+Words = Words()  # singleton
 
 def getWordAssocs(word, n):
-	nounfile = open("nouns")
-	nounlist = [x for x in nounfile.read().splitlines()]
-	nounfile.close()
+    """ Return the n most frequent nouns in the wiki article for 'word'
+    """
+    wikitext = wikipedia.getArticle(word)
+    wordCounts = {}
+    if wikitext == False:
+        return False
+    for word in wikitext.split():
+        # Lowercase and remove punctuation
+        word = word.lower()
+        word = word.translate(string.maketrans("", ""), string.punctuation)
 
-	wikitext = wikipedia.getArticle(word)
-	wordlist = []
-	if wikitext == False:
-		return False
-	for words in wikitext.split():
-		words = words.lower()
-		words = words.translate(string.maketrans("", ""), string.punctuation)
-		if nounlist.count(words) > 0:
-			if [x[1] for x in wordlist].count(words) == 0:
-				wordlist.append((1, words))
-			else:
-				i = [x[1] for x in wordlist].index(words)
-				t = wordlist[i] # tuple
-				wordlist[i] = (t[0]+1, t[1])
-	wordlist.sort()
-	wordlist.reverse()
-	return [x[1] for x in wordlist[:n]]
+        if word in Words.nouns:
+            if word not in wordCounts:
+                wordCounts[word] = 1
+            else:
+                wordCounts[word] += 1
+    # Return the n most frequent words
+    items = [(v, k) for k, v in wordCounts.items()]
+    items.sort()
+    items.reverse()
+    return [x[1] for x in items[:n]]
 
-def getRandomNoun():
-	nounfile = open("nouns")
-	nounlist = [x for x in nounfile.read().splitlines()]
-	nounfile.close()
+def getNounPuns(word1, word2):
+    """ Given two nouns, return a list of puns
+    """
+    punlist = []
+    try:
+        phenword1 = Words.phonemes(word1) # phenword1 = ("B OW L")
+        phenword2 = Words.phonemes(word2) # phenword2 = ("F IH SH B OW L")
+    except KeyError:
+        return []
+    lword1 = phenword1.split() # lword1 = ("B", "OW", "L")
+    lword2 = phenword2.split() # lword2 = ("F", "IH", "SH", "B", "OW", "L")
 
-	return nounlist[int(math.floor(random.random() * len(nounlist)))]
-
-def badjoke():
-	ASSOC_COUNT = 5	
-
-	word1 = getRandomNoun()
-	word2 = getRandomNoun()
-	print("What do you get when you cross a " + word1 + " with a " + word2 + "?")
-
-	wordassocslist1 = getWordAssocs(word1, ASSOC_COUNT)
-	wordassocslist2 = getWordAssocs(word2, ASSOC_COUNT)
-
-	if wordassocslist1 == False or wordassocslist2 == False or len(wordassocslist1) == 0 or len(wordassocslist2) == 0:
-		print("JOKE FAILED, ABORT, ABORT")
-		return
-
-	print([getPhenomes(x) for x in wordassocslist1])
-	print([getPhenomes(x) for x in wordassocslist2])
-	
-#	wordassocs1 = wordassocslist1[int(math.floor(random.random() * len(wordassocslist1)))]
-#	wordassocs2 = wordassocslist2[int(math.floor(random.random() * len(wordassocslist2)))]
-
-#	print("A " + wordassocs1 + wordassocs2)
-
-def getNounPuns(list1, list2):
-	punlist = []
-	for word1 in list1: # word1 = "bowl"
-		for word2 in list2: # word2 = "fishbowl"
-			phenword1 = getPhenomes(word1) # phenword1 = ("B OW L")
-			phenword2 = getPhenomes(word2) # phenword2 = ("F IH SH B OW L")
-			lword1 = phenword1.split() # lword1 = ("B", "OW", "L")
-			lword2 = phenword2.split() # lword2 = ("F", "IH", "SH", "B", "OW", "L")
-
-			# check prefixes word1, suffixes word2
-			for phonemeCount in range(MIN_PHENOMES_SHARED, len(lword1) + 1): # phonemeCount = 2, 3
-				newword = string.join(lword1[:phonemeCount]) # newword = "B OW"
-				ind = string.find(phenword2, newword, 1)
-				if ind != -1 and ind + len(newword) == len(phenword2):
-					# pun found!
-					punlist.append(word2 + pruneNoun(word1, newword))
-			# check prefixes word2, suffixes word1
-			for phonemeCount in range(MIN_PHENOMES_SHARED, len(lword2) + 1):
-				newword = string.join(lword2[:phonemeCount])
-				ind = string.find(phenword1, newword, 1)
-				if ind != -1 and ind + len(newword) == len(phenword1):
-					# pun found!
-					punlist.append(word1 + pruneNoun(word2, newword))
-	return punlist
+    # check prefixes word1, suffixes word2
+    for phonemeCount in range(MIN_PHENOMES_SHARED, len(lword1) + 1): # phonemeCount = 2, 3
+        newword = string.join(lword1[:phonemeCount]) # newword = "B OW"
+        ind = string.find(phenword2, newword, 1)
+        if ind != -1 and ind + len(newword) == len(phenword2):
+            # pun found!
+            punlist.append(word2 + pruneNoun(word1, newword))
+    # check prefixes word2, suffixes word1
+    for phonemeCount in range(MIN_PHENOMES_SHARED, len(lword2) + 1):
+        newword = string.join(lword2[:phonemeCount])
+        ind = string.find(phenword1, newword, 1)
+        if ind != -1 and ind + len(newword) == len(phenword1):
+            # pun found!
+            punlist.append(word1 + pruneNoun(word2, newword))
+    return punlist
 
 def isConsonant(letter):
-	if CONSONANTS.count(letter) > 0:
-		return True
-	else:
-		return False
+    return letter in CONSONANTS
 
 def isVowel(letter):
-	if VOWELS.count(letter) > 0:
-		return True
-	else:
-		return False
+    return letter in VOWELS
 
 def getConsonantCount(word):
-	consonantCount = 0
-	for letter in list(word):
-		if isConsonant(letter):
-			consonantCount += 1
-	return consonantCount
+    return len(filter(isConsonant, list(word)))
 
 def pruneNoun(word, phonemes):
-	phonemes.replace(" ", "")
+    """ Cut the first syllable... sort of
+    """
+    phonemes.replace(" ", "")
 
-	consonants = getConsonantCount(phonemes)
+    consonants = getConsonantCount(phonemes)
 
-	consonantsCounted = 0
-	cutindex = -1
-	for letter in list(word):
-		cutindex += 1
-		if isConsonant(letter):
-			consonantsCounted += 1
-			if consonantsCounted >= consonants:
-				break
-	return word[cutindex:]
+    consonantsCounted = 0
+    cutindex = -1
+    for letter in list(word):
+        cutindex += 1
+        if isConsonant(letter):
+            consonantsCounted += 1
+            if consonantsCounted >= consonants:
+                break
+    return word[cutindex:]
 
 def conjunctify(word):
-	if isVowel(word[0]):
-		return "n " + word
-	else:
-		return " " + word
+    """ Prefix 'n ' if the word starts with a vowel
+    """
+    if isVowel(word[0]):
+        return "n " + word
+    else:
+        return " " + word
 
-def main():
-	result = ""
-	while True:
-		noun1 = getRandomNoun()
-		noun2 = getRandomNoun()
-		list1 = [noun1]
-		list2 = [noun2]
-		punlist = getNounPuns(list1, list2)
-		if punlist != []:
-			jokenoun1 = getWordAssocs(noun1, 1)[0]
-			jokenoun2 = getWordAssocs(noun2, 1)[0]
-			result += "What do you get when you cross a" + conjunctify(jokenoun1) + " with a" + conjunctify(jokenoun2) + "? "
-			result += "A" + conjunctify(punlist[0]) + "!"
-			break
-	print(result)
-main()
+def makeJoke():
+    joke = ""
+    while True:
+        noun1 = Words.getRandomNoun()
+        noun2 = Words.getRandomNoun()
+        punlist = getNounPuns(noun1, noun2)
+        if punlist:
+            jokenoun1 = getWordAssocs(noun1, 1)[0]
+            jokenoun2 = getWordAssocs(noun2, 1)[0]
+            joke += "What do you get when you cross a{0} with a{1}? ".format(conjunctify(jokenoun1), conjunctify(jokenoun2))
+            joke += "A" + conjunctify(punlist[0]) + "!"
+            break  # ... we're done here
+    return joke
+
+if __name__ == "__main__":
+    print(makeJoke())
